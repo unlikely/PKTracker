bca17b483df436fcc3879eaaff14dffabfa7c226
Patrick Kennedy <bigredtriangle@gmail.com>
2011-12-06 15:54:19 -0500

    basic create new Developer and display stats functionality

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -2,4 +2,21 @@ class DevelopersController < ApplicationController
   def index
     @developer = Developer.all
   end
+
+  def new
+    @developer = Developer.new
+  end
+
+  def create
+    @developer = Developer.new(params[:developer])
+    if @developer.save
+      redirect_to developer_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+    @developer = Developer.find params[:id]
+  end
 end
--- spec/controllers/developers_controller_spec.rb
+++ spec/controllers/developers_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe DevelopersController do
+  describe 'index' do
+    it 'works' do
+      get :index
+      response.should be_success
+    end
+  end
 
+  describe 'create' do
+    it 'redirects to show' do
+      post :create, ev: Factory(:developer, name: 'usar')
+    end
+  end
+
+  describe 'show' do
+    it 'works' do
+      ev = Factory :developer, name: 'usar'
+      get :show, id: ev.to_param
+      response.should be_success
+    end
+  end
 end
