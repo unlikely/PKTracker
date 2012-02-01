2ad1d9bafb14d8a60bbe77cc02d4958437ea3bd7
Scott Conley <scott@14to9.org>
2012-02-01 16:39:39 -0500

    narrowed developer metric box

--- app/helpers/developers_helper.rb
+++ app/helpers/developers_helper.rb
@@ -2,10 +2,7 @@ module DevelopersHelper
   def score_label(score)
     score_tag = score.upcase
     content_tag(:div, class: "score-description") do
-      case score
-        when Score::Win then score_tag << '!'
-        else score_tag
-      end
+      I18n.t("score.labels.#{score_tag}")
     end
   end
 
